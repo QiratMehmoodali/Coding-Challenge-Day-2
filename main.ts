e9852e69cad31cadbdfdba7fd76d02f72558c888
QiratMehmoodali <162498646+QiratMehmoodali@users.noreply.github.com>
2024-03-08 02:43:54 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//Q1:
+//console.log("Nelson Mandala once said",'"Education is the most powerful weapon which you can use to change the world"');
+//Q2:
+//let famousePerson="Nelson Mandala";
+//let message="once said,'Education is the most powerful weapon which you can use to change the world'";
+//console.log(famousePerson,message);
+//Q3:
+let whiteSpace="\n\t Qirat Mehmood Ali\t\n";
+console.log(whiteSpace);
+let withoutwhiteSpace=whiteSpace.trim();
+console.log(withoutwhiteSpace);
